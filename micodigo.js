8029d845ad7630eecd5cac1f88c6ea2b76482ae7
Alejandro Hidalgo <alex_x98@hotmail.com>
2021-03-29 22:17:43 -0500

    creación de un javascript externo para manejar mejor los codigos

---
+++ micodigo.js
@@ -0,0 +1,12 @@
+function mostraralerta(){ //Se crea una función "mostraralerta"
+alert('hizo clic'); //y esta mantiene la accion de aletra
+                    //con el mensaje marcado
+}
+function hacerclic(){ //Se crea otra función "hacer clic"
+/*Aquí se explica que el elemento "p" que está en la posición 0
+dentro del arreglo (primer elemento de texto) será ejecutada y manda a llamar
+a la función "mostraralerta" */
+document.getElementsByTagName('p')[0].onclick=mostraralerta;
+
+}
+window.onload=hacerclic;
