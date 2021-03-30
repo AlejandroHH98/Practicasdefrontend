2ea8b3d9b667f33c6f41280133a3435bb87fac04
Alejandro Hidalgo <alex_x98@hotmail.com>
2021-03-30 14:40:53 -0500

    Usando querySelectorAll

--- micodigo.js
+++ micodigo.js
@@ -1,14 +1,15 @@
-function mostraralerta(){ //Se crea una función "mostraralerta"
-alert('hizo clic'); //y esta mantiene la accion de aletra
-                    //con el mensaje marcado
-}
-function hacerclic(){ //Se crea otra función "hacer clic"
+//Se crea otra función "hacer clic"
 /*Aquí se explica que el elemento "p" que está en la posición 0
 dentro del arreglo (primer elemento de texto) será ejecutada y manda a llamar
 a la función "mostraralerta" */
-/*querySelector retorna el primer elemento que concuerta con el grupo de selectores
-especificiados entre parentesis*/
-document.querySelector("#principal p:first-child").onclick=mostraralerta;
-
+function hacerclic(){ 
+var lista=document.querySelectorAll("#principal p");/*querySelectorAll retorna todos los elementos que concuerdan con el grupo de selectores
+declarados en parentesis*/
+lista[0].onclick=mostraralerta; //El arreglo lista muestra el primer elemento "p"
 }
+function mostraralerta(){ //Se crea una función "mostraralerta"
+alert('hizo clic'); //y esta mantiene la accion de alerta
+                    //con el mensaje marcado
+}
+
 window.onload=hacerclic; //Ejecuta la funcion "hacerclic" cuando el documento se hayan terminado
