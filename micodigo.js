18075ec04f704f24beb44054adce17fb6d6467fa
Alejandro Hidalgo <alex_x98@hotmail.com>
2021-03-30 15:07:50 -0500

    modificiacion de comentarios

--- micodigo.js
+++ micodigo.js
@@ -4,9 +4,10 @@ dentro del arreglo (primer elemento de texto) será ejecutada y manda a llamar
 a la función "mostraralerta" */
 function hacerclic(){ 
 var lista=document.getElementById("principal").querySelectorAll("p");/* Por medio de getElementById se precisa
-al identificador que queremos y con queryselector el tipo de elemnto que queremos aplicarle una función
+al identificador que queremos y con querySelectorAll el tipo de elemento que queremos aplicarle una función
 en este caso al elemento "p"*/
-lista[0].onclick=mostraralerta; //Todos los elementos p dentro del identificador mostrarán un mensaje de alerta
+lista[0].onclick=mostraralerta; //El elemento dentro del arreglo que se muestre es la posicion del elemento que mostrará la alerta
+                                //En este caso es el elemento 0 es decir el primer "p"
 }
 function mostraralerta(){ //Se crea una función "mostraralerta"
 alert('hizo clic'); //y esta mantiene la accion de alerta
