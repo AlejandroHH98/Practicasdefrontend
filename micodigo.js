fc0c4195cdd77a21cd5ae6076400c78161031b64
Alejandro Hidalgo <alex_x98@hotmail.com>
2021-03-30 14:29:47 -0500

    Cambio getelementsbytagname a querySelector

--- micodigo.js
+++ micodigo.js
@@ -6,7 +6,9 @@ function hacerclic(){ //Se crea otra función "hacer clic"
 /*Aquí se explica que el elemento "p" que está en la posición 0
 dentro del arreglo (primer elemento de texto) será ejecutada y manda a llamar
 a la función "mostraralerta" */
-document.getElementsByTagName('p')[0].onclick=mostraralerta;
+/*querySelector retorna el primer elemento que concuerta con el grupo de selectores
+especificiados entre parentesis*/
+document.querySelector("#principal p:first-child").onclick=mostraralerta;
 
 }
 window.onload=hacerclic; //Ejecuta la funcion "hacerclic" cuando el documento se hayan terminado
