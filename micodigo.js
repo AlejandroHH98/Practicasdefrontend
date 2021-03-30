a38d4b4aa751ba513e0addb11b280ba4c5e0617b
Alejandro Hidalgo <alex_x98@hotmail.com>
2021-03-30 14:58:50 -0500

    Utilizando un bucle FOR

--- micodigo.js
+++ micodigo.js
@@ -5,11 +5,15 @@ a la función "mostraralerta" */
 function hacerclic(){ 
 var lista=document.querySelectorAll("#principal p");/*querySelectorAll retorna todos los elementos que concuerdan con el grupo de selectores
 declarados en parentesis*/
-lista[0].onclick=mostraralerta; //El arreglo lista muestra el primer elemento "p"
+
+for(var f=0; f<lista.length; f++){ //Bucle for para crear un arreglo de los elementos p
+lista[f].onclick=mostraralerta; //Todos los elementos p dentro del identificador mostrarán un mensaje de alerta
+
+    }
 }
 function mostraralerta(){ //Se crea una función "mostraralerta"
 alert('hizo clic'); //y esta mantiene la accion de alerta
                     //con el mensaje marcado
-}
+} 
 
 window.onload=hacerclic; //Ejecuta la funcion "hacerclic" cuando el documento se hayan terminado
